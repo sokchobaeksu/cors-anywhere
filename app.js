81c34b4d37aee5cd028b5745fb69cf2f1f3af493
sokchobaeksu <113843622+sokchobaeksu@users.noreply.github.com>
2022-11-12 06:12:19 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const ajax = new XMLHttpRequest();
-const NEWS_URL = 'https://sockcho.herokuapp.com/https://api.hnpwa.com/v0/news/1.json';
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
